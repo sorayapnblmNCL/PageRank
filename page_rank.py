3f7ff53fd704e68ffdbbe9969d65cd873d4a2f1d
sorayapanambalom <S.V.Panambalom2@newcastle.ac.uk>
2022-12-15 18:47:23 +0000

    Add comments

--- page_rank.py
+++ page_rank.py
@@ -1,15 +1,15 @@
 import sys
 import os
-import time
 import argparse
-from progress import Progress
-import random
+
+#Import the needed functions from the modules
+from random import choice
+from time import time
 
 
 
 def load_graph(args):
     """Load graph from text file
-
     Parameters:
     args -- arguments named tuple
 
@@ -18,8 +18,11 @@ def load_graph(args):
     """
     # Create a graph
     graph = dict()
-    # Iterate through the file line by line
-    for line in args.datafile:
+    # Store constants in local variables
+    file = args.datafile
+
+    # Iterate through the file line by line>W
+    for line in file:
         # And split each line into two URLs
         node, target = line.split()
         if node not in graph:
@@ -28,7 +31,6 @@ def load_graph(args):
         elif node in graph:
             graph[node].append(target)
     return graph
-
 
 
 def print_stats(graph):
@@ -42,128 +44,86 @@ def print_stats(graph):
     return f'The number of nodes is {number_nodes} and the number of edges is {count}'
 
 
-
 def stochastic_page_rank(graph, args):
     """Stochastic PageRank estimation
 
-        Parameters:
-        graph -- a graph object as returned by load_graph()
-        args -- arguments named tuple
+    Parameters:
+    graph -- a graph object as returned by load_graph()
+    args -- arguments named tuple
 
-        Returns:
-        A dict that assigns each page its hit frequency
+    Returns:
+    A dict that assigns each page its hit frequency
 
-        This function estimates the Page Rank by counting how frequently
-        a random walk that starts on a random node will after n_steps end
-        on each node of the given graph.
-        """
-
+    This function estimates the Page Rank by counting how frequently
+    a random walk that starts on a random node will after n_steps end
+    on each node of the given graph.
+    """
     # Initialize the hit count frequency of every node to 0
-    dict_count = dict()
-    for i in graph:
-        dict_count[i] = 0
+    # Use dict comprehension for optimization
+    dict_count = {i: 0 for i in graph}
+
+    # Store constants in local variables
+    n_repetitions = args.repeats
+    n_steps = args.steps
 
     # Repeat the random walker process "args.repeats" times
-    for k in range(args.repeats):
+    for k in range(n_repetitions):
         # Choose a random source node in the graph and increase its hit count
-        current_node = random.choice(list(graph))
+        current_node = choice(list(graph))
         # Do a random walk of "args.steps" steps
-        for j in range(args.steps):
+        for j in range(n_steps):
             # Select randomly a node from the target list of current_node and name it as the new current
-            current_node = random.choice(graph[current_node])
-            # If the new current_node does not have has no outgoing edges then break the walker steps loop
-            # to select a new random node
+            current_node = choice(graph[current_node])
+            # If the new current_node does not have has no outgoing edges then break the walker steps loop to select
+            # a new random node
             if current_node not in graph:
                 break
         # Increase the hit value of the finale node reached from the random walk
-        dict_count[current_node] += 1 / args.repeats
+        dict_count[current_node] += 1 / n_repetitions
 
     return dict_count
-
-
-
-
-"""""
-if len(graph[random_node_source]) == 0:
-    random_node_source = random.choice(list(graph))
-    dict_count[random_node_source] += 1 / (args.repeats)
-    
-    else:
-        random_node_source = random.choice(graph[random_node_source])
-        dict_count[random_node_source] += 1/(args.repeats)
-
-=============
-
-#Choose a target nodes in the random node's  edges
-    for k in range(args.repeats):
-        # Choose a random source node in the graph and increase its hit count
-        random_node_source = random.choice(list(graph))
-        dict_count[random_node_source] += 1 / (args.repeats)
-        if len(graph[random_node_source]) != None: #0 or None here
-            random_node_source = random.choice(graph[random_node_source])
-            dict_count[random_node_source] += 1 / (args.repeats)
-
-    return dict_count
-    
-"""""
 
 
 
 def distribution_page_rank(graph, args):
     """Probabilistic PageRank estimation
+
     Parameters:
     graph -- a graph object as returned by load_graph()
     args -- arguments named tuple
+
     Returns:
     A dict that assigns each page its probability to be reached
+
     This function estimates the Page Rank by iteratively calculating
     the probability that a random walker is currently on any node.
     """
+    # Store constants in local variables
+    n_steps = args.steps
+    number_nodes = len(graph)
+
     # Initialize each node probability to 1/(number of nodes)
-    node_prob = dict()
-    for node in graph:
-        node_prob[node] = 1/ (len(graph))
+    # Use dict comprehension for optimization
+    node_prob = {node: 1/number_nodes for node in graph}
 
     # Repeat iterations "args.steps" times
-    for i in range(args.steps):
+    for i in range(n_steps):
 
-        #Initialize the next_prob of each node to 0
-        next_prob = dict()
-        for node in graph:
-            next_prob[node] = 0
+        # Initialize the next_prob of each node to 0
+        next_prob = {node: 0 for node in graph}
 
         for node in graph:
-            p = node_prob[node] /(len(graph[node]))
+            # Store constant number in  variable
+            number_target = len(graph[node])
+            p = node_prob[node] / number_target
             for target in graph[node]:
                 next_prob[target] += p
-
-        for node in next_prob:
-            node_prob[node] = next_prob[node]
+        node_prob = next_prob
 
     return node_prob
 
 
-
-
-
-
-"""
-initialize node_prob[node] = 1/(number of nodes) for all nodes
-repeat n_steps times:
-    initialize next_prob[node] = 0 for all nodes
-    for each node:
-        p <- node_prob[node] divided by its out degree
-        for each target among out edges of node:
-            next_prob[target] += p
-    node_prob <- next_prob
-    
-"""
-
-
-
-
-
-
+# Implementations of argparse command line arguments
 parser = argparse.ArgumentParser(description="Estimates page ranks from link information")
 parser.add_argument('datafile', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
                     help="Textfile of links among web pages as URL tuples")
@@ -175,28 +135,18 @@ parser.add_argument('-n', '--number', type=int, default=20, help="number of resu
 
 
 if __name__ == '__main__':
+
+    # Instantiate the parser
     args = parser.parse_args()
     algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
 
     graph = load_graph(args)
-    #print(graph)
-
-    #result = '\n'.join(f'{key}: {value}' for key, value in graph.items())
-    #print(result)
-
-    print(print_stats(graph))
-    #print(list(graph))
-
-    #print(stochastic_page_rank(graph, args))
-
-
 
     print_stats(graph)
 
-
-    start = time.time()
+    start = time()
     ranking = algorithm(graph, args)
-    stop = time.time()
+    stop = time()
     time = stop - start
 
     top = sorted(ranking.items(), key=lambda item: item[1], reverse=True)
